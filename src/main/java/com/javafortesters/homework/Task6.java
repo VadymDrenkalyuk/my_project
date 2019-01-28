4f069c286d56e02925192ac2edd0cff4639a20c1
vdren <vdren@softserveinc.com>
2019-01-28 19:01:02 +0200

    Added condition "==" to Task6 Changed the number of digits in Task8

--- src/main/java/com/javafortesters/homework/Task6.java
+++ src/main/java/com/javafortesters/homework/Task6.java
@@ -2,8 +2,8 @@ package com.javafortesters.homework;
 
 public class Task6 {
     public static void main(String[] args) {
-        int n = 13;
-        int m = 5;
+        int n = 7;
+        int m = 13;
         int number = 10;
 
         int firstDelta = number - n;
@@ -11,8 +11,14 @@ public class Task6 {
 
         if (Math.abs(firstDelta) > Math.abs(secondDelta)) {
             System.out.println("m is closer to your number");
-        } else {
+        }
+
+        if (Math.abs(firstDelta) < Math.abs(secondDelta)) {
             System.out.println("n is closer to your number");
+        }
+
+        if (Math.abs(firstDelta) == Math.abs(secondDelta)) {
+            System.out.println("m and n is equally close to your number");
         }
     }
 }
--- src/main/java/com/javafortesters/homework/Task8.java
+++ src/main/java/com/javafortesters/homework/Task8.java
@@ -5,10 +5,9 @@ public class Task8 {
         double a = 2;
         int b = 0;
 
-        while (b<=20) {
+        while (b<=19) {
             System.out.println(Math.scalb(a, b));
-            b ++;
+            b++;
         }
-
     }
 }
