6516be5cb7c8a66aabd13005ce0f05aa7ea8c063
vdren <vdren@softserveinc.com>
2019-01-28 15:38:05 +0200

    Added addition with scanner class

--- src/main/java/com/javafortesters/homework/Task8.java
+++ src/main/java/com/javafortesters/homework/Task8.java
@@ -1,5 +1,14 @@
 package com.javafortesters.homework;
 
 public class Task8 {
+    public static void main(String[] args) {
+        double a = 2;
+        int b = 0;
 
+        while (b<=20) {
+            System.out.println(Math.scalb(a, b));
+            b ++;
+        }
+
+    }
 }
