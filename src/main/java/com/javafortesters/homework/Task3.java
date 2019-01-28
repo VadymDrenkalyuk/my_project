a833c814804ded303257b83c453b7abb07174a0d
vdren <vdren@softserveinc.com>
2019-01-28 16:33:46 +0200

    Added addition with scanner class

---
+++ src/main/java/com/javafortesters/homework/Task3.java
@@ -0,0 +1,10 @@
+package com.javafortesters.homework;
+
+public class Task3 {
+    public static void main(String[] args) {
+        float x = 43.321f;
+        float y = 45.12f;
+        System.out.println(Math.round(x));
+        System.out.println(Math.round(y));
+        }
+}
