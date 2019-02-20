10db327942edd5ef646bf7ccd044e3415dbd9fa6
vdren <vdren@softserveinc.com>
2019-02-20 18:58:59 +0200

    Added task 2 from 6 (homework 2)

---
+++ src/main/java/com/javafortesters/homework/Task14.java
@@ -0,0 +1,16 @@
+package com.javafortesters.homework;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Task14 {
+    public static void main(String[] args) {
+        List<Integer> list = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11);
+        for (int i = 0; i < list.size(); i++) {
+            if (i % 2 == 0) {
+                System.out.println(list.get(i));
+            }
+        }
+    }
+}
+
