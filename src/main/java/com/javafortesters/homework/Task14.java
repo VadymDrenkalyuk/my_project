8c3c0783e9d9033ec5d963f8325a2c1a9a4d41a8
vdren <vdren@softserveinc.com>
2019-02-21 18:51:19 +0200

    Updated without if.

--- src/main/java/com/javafortesters/homework/Task14.java
+++ src/main/java/com/javafortesters/homework/Task14.java
@@ -6,10 +6,8 @@ import java.util.List;
 public class Task14 {
     public static void main(String[] args) {
         List<Integer> list = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11);
-        for (int i = 0; i < list.size(); i++) {
-            if (i % 2 == 0) {
-                System.out.println(list.get(i));
-            }
+        for (int i = 0; i < list.size(); i += 2) {
+            System.out.println(list.get(i));
         }
     }
 }
