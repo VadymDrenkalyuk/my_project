c6391df2fc333453883248701328acf1c787f012
vdren <vdren@softserveinc.com>
2019-02-26 01:32:24 +0200

    used method isNumberPresent

--- src/main/java/com/javafortesters/homework/Task16.java
+++ src/main/java/com/javafortesters/homework/Task16.java
@@ -7,16 +7,31 @@ import java.util.Map;
 
 
 public class Task16 {
+    static Map<String, List> map;
+
     public static void main(String[] args) {
         List<Integer> evenList = Arrays.asList(2, 4, 6, 8, 10);
         List<Integer> oddList = Arrays.asList(1, 3, 5, 7, 9);
-        Map<String, List> map = new HashMap<>();
+        map = new HashMap<>();
 
         map.put("even", evenList);
         map.put("odd", oddList);
 
-        System.out.println(map.get("even").contains(5));
-        System.out.println(map.get("odd").contains(9));
+        map.get("even").contains(5);
+        map.get("odd").contains(18);
+
+        System.out.println(isNumberPresent(105456));
 
     }
+
+    public static boolean isNumberPresent(int n) {
+        if (n % 2 == 0) {
+            return map.get("even").contains(n);
+        } else {
+            return map.get("odd").contains(n);
+        }
+    }
+
+
+
 }
